758175a8fb0cefe34fe712336db38b0504e69ddd
MaZiayee <maziayee@gmail.com>
2025-02-09 01:04:04 +0330

    add Class Category

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Category
 # Register your models here.
 
 
@@ -9,4 +9,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('id','author','title' ,'counted_views','status','published_date','created_date')
     list_filter = ['status','author']
     search_fields = ['title','content']
+
 admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
