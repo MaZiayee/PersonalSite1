405da6c1dc26fe2c0d77a185fb4997fd0d3c5d4a
MaZiayee <maziayee@gmail.com>
2025-02-24 22:21:37 +0330

    contact form
    connect Contact form to models

--- website/views.py
+++ website/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from blog.models import Post
+from website.models import Contact
+from website.forms import ContactForm
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     posts = Post.objects.filter(status=1)[:3]
-    context = {'posts':posts}
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request,messages.SUCCESS,'متشکر از پیام شما! بزودی با شما تماس خواهم گرفت.')
+            return redirect("website:homepage")
+        else:
+            messages.add_message(request,messages.ERROR,'مشکلی پیش آمده! لطفا مجدد تلاش کنید.')
+            return redirect("website:homepage")
+    form = ContactForm()
+    print(messages)
+
+    context = {'posts':posts, 'form':form}
     return render(request,'website/index.html',context)
+
 
