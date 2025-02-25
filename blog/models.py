1818c340a3a8bcf3c5a29b9da9b6f23f65238cdd
MaZiayee <maziayee@gmail.com>
2025-02-25 14:05:44 +0330

    robots and tags

--- blog/models.py
+++ blog/models.py
@@ -9,13 +9,19 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    # tag 
+    tag = models.ManyToManyField(Tag)
     category = models.ManyToManyField(Category)
     status = models.BooleanField(default=False)
     counted_views = models.IntegerField(default=0)
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,12 +40,18 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'robots',
 
     'website',
     'blog',
 ]
 
+# sites framework
 SITE_ID = 2
+
+# robots
+ROBOTS_USE_SITEMAP = False
+ROBOTS_USE_HOST = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
